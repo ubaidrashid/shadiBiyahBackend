7d04b8d18c3d37e7c1595ce70d5afbbb0c2f15be
MUHAMMAD UBAID BIN RASHID <ubaidqadri97@gmail.com>
2025-05-09 04:00:37 +0500

    dbjs

--- config/db.js
+++ config/db.js
@@ -5,14 +5,16 @@ dotenv.config();
 
 const connectDB = async () => {
     const mongoURI = process.env.MONGO_URI || 'mongodb+srv://ubaidqadri97:ubaid1234rty@ubaidcluster.zmbximk.mongodb.net/shadibiyah?retryWrites=true&w=majority&appName=ubaidcluster'; // Default URI for local MongoDB
-    console.log("Connecting to MongoDB..." , process.env.MONGO_URI);
+
+    // Log the URI being used to connect
+    console.log("Connecting to MongoDB using URI:", mongoURI);
+
     try {
-        await mongoose.connect(mongoURI);
-        console.log("Connecting to MongoDB..." , process.env.MONGO_URI);
+        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("✅ MongoDB connected");
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
-  }
+    } catch (error) {
+        console.error("❌ MongoDB connection error:", error.message);
+    }
 };
 
 export default connectDB;
