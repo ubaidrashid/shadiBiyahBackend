cb7130d31a6361cb3144329eaea9bad83f1c069f
MUHAMMAD UBAID BIN RASHID <ubaidqadri97@gmail.com>
2025-05-09 03:56:46 +0500

    dbjs

--- config/db.js
+++ config/db.js
@@ -4,9 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
+    const mongoURI = process.env.MONGO_URI || 'mongodb+srv://ubaidqadri97:ubaid1234rty@ubaidcluster.zmbximk.mongodb.net/shadibiyah?retryWrites=true&w=majority&appName=ubaidcluster'; // Default URI for local MongoDB
+    console.log("Connecting to MongoDB..." , process.env.MONGO_URI);
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connecting to MongoDB..." , process.env.MONGO_URI);
+        console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error.message);
   }
