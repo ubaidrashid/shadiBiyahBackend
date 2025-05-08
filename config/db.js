7a9be4b6960969b866e6754335287be9948292b9
MUHAMMAD UBAID BIN RASHID <ubaidqadri97@gmail.com>
2025-05-09 03:40:29 +0500

    db js update

--- config/db.js
+++ config/db.js
@@ -3,16 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// console.log("MONGO_URI:", process.env.MONGO_URI); // Debugging line
-
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("url" , process.env.MONGO_URI);
-        console.log("MongoDB connected ✅");
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+  }
+};
 
 export default connectDB;
