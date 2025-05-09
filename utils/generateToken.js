d4f4942354ebc2d7af45fb7ab8e585b016b5b078
MUHAMMAD UBAID BIN RASHID <ubaidqadri97@gmail.com>
2025-05-09 15:21:30 +0500

    generate

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,10 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const generateToken = (userId) => {
-
+console.log(process.env.JWT_SECRET_KEY , "secret key")
     return jwt.sign({ id: userId }, process.env.JWT_SECRET_KEY, { expiresIn: '30d' })
 
 }
