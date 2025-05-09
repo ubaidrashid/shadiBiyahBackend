38c70f4b3b3a10f69d7c433d418037250500c190
MUHAMMAD UBAID BIN RASHID <ubaidqadri97@gmail.com>
2025-05-09 16:59:27 +0500

    db cont route

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId) => {
-  const secretKey = 'ubbiqadri97'; // ✅ hardcoded
+  const secretKey = process.env.JWT_SECRET_KEY; // ✅ hardcoded
   console.log(secretKey, 'secret key');
 
   return jwt.sign({ id: userId }, secretKey, { expiresIn: '30d' });
